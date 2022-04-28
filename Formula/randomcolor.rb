b2ce73b7b84b4f7aa12adb420002c6194713cfa4
Elie <79168733+el2zay@users.noreply.github.com>
2022-04-28 19:38:26 +0200

    Update randomcolor.rb

--- Formula/randomcolor.rb
+++ Formula/randomcolor.rb
@@ -2,7 +2,7 @@ class Randomcolor < Formula
     desc "Display a random color in a terminal and convert HEX<->RGB. Only French is available at this time - CLI"
     homepage "https://github.com/el2zay/random-color-cli"
     url "https://github.com/el2zay/random-color-cli/releases/download/v0.1.1/randomcolor.tar.gz"
-    sha256 "404558c841277f1dbf874ce14dcb1dfb4eda93e7d09236058c74e9612956e7d3"
+    sha256 "9b1dd8cb5f11c2de9695c90f37a956cafd08fcf897057ddc25dba84b7c0832df"
     license "MIT"
     version "0.1.1"
   
